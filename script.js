be5fc2b6f26d7784a77bf3381125ae8ec40fbaae
jakenlightbringer <mememe49bf4@gmail.com>
2023-08-26 00:37:37 -0700

    started player choice logic

--- script.js
+++ script.js
@@ -16,11 +16,44 @@ const gameController = (() =>{
     }
     
 })();
-const Player = (identity, selection) =>{
-    const getIdentity = () => identity;
-    const getSelection = () => selection;
+/*
+create play game function
+Have player1 X and player 2 O and switch between the 2
+Logic created for finding 3 in a row
+If 3 in a row unable// all tiles filled
+button popup rematch? reset board
+Win logic and button popup for winlogic as well
+CSS last 
+*/
+
+playButton = document.getElementById('play-button');
+playButton.addEventListener("click", (e)=>{
+    assignPlayer();
+    playButton.remove();
+});
+
+
+function assignPlayer(){
+    let playerButtonContainer = document.getElementById("playerSelection");
+    let numButton = 2;
+
+    for(let i = 0; i < numButton; i++){
+        let playerButtonChoice1 = document.createElement('button');
+        playerButtonChoice1.textContent = "Player " + [i+1];
+
+        playerButtonChoice1.addEventListener('click', (e) =>{
+            if(playerButtonChoice1.innerHTML == "Player 1"){
+                const selection = 'X';
+                return selection
+            }
+            else if(playerButtonChoice1.innerHTML == "Player 2"){
+                const selection = "O";
+                return selection
+            }
+        });
+    
+    
+    playerButtonContainer.appendChild(playerButtonChoice1);
+    }
+
 }
-
-let playerSelection 
-
-
