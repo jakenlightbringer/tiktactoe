d5d954b46ff59b7fbf3da4b5dae3c9f846350f05
jakenlightbringer <mememe49bf4@gmail.com>
2023-08-25 21:51:26 -0700

    player input

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 const gameController = (() =>{
-    const board = ["X", "X","O","X","","","","O","",];
+    const board = ["", "","","","","","","","",];
 
     const getBoard = () => board;
     let boardContainer= document.getElementById('game-container')
     for(let i =0; i < board.length; i++){
         let buttonBoard = document.createElement('button');
+        buttonBoard.classList.add("game-button");
+        buttonBoard.addEventListener('click', (e)=>{
+            buttonBoard.textContent = "x";
+            buttonBoard.disabled = "true";
+        });
         buttonBoard.textContent = board[i];
         boardContainer.appendChild(buttonBoard);
+        
     }
+    
 })();
+const Player = (identity, selection) =>{
+    const getIdentity = () => identity;
+    const getSelection = () => selection;
+}
+
+let playerSelection 
+
+
